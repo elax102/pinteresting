8a4ec404aef1031dbe432acbdf99614c689c93c2
Alex Daivs <davis.alex.jr@gmail.com>
2015-09-17 11:16:43 -0400

    Changed Paperclip-AWS config, trying to resolve 'missing bucket' problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,10 +82,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://alex-onemonth-rails.herokuapp.com/' }
 
   # Set paperclip to upload images to Amazon S3
-  config.paperclip_defaults = {
+   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_protocol => 'http',
+    :bucket => ENV['AWS_BUCKET'],
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
